f97880d7c2175ed8ed13b4052e12984ec404a1b2
Mustafa <mostafaayyan1@gmail.com>
2026-05-24 18:17:20 +0300

    Decleration Variabls in Person Classs

---
+++ المشكلة 40 من الكورس العاشر/المشكلة 40 من الكورس العاشر.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "clsPerson.h";
+#include "clsEmploee.h";
+using namespace std;
+
+int main()
+
+{
+
+    clsEmployee Employee1(10, "Mohammed", "Abu-Hadhoud", "A@a.com", "8298982", "CEO", "ProgrammingAdvices", 5000);
+    
+    Employee1.Print();
+
+    system("pause>0");
+    return 0;
+}
